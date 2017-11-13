fd6fa8eb18bf059559c6a754b2b748891a7934dd
Ahmad Fahmi <afahmi3@yahoo.co.id>
2017-11-13 19:35:03 +0700

    Update User.h

--- User.h
+++ User.h
@@ -6,10 +6,23 @@
 #define BATTLEFOROLYMPIA_USER_H
 
 #include "Unit.h"
+#include "Map.h"
+
+#define STARTING_GOLD 100
+#define STARTING_INCOME 100
 
 typedef struct {
-    int id, gold, income, upkeep, color;
-    Unit* units;
+    int gold, income, upkeep, color;
+    lcList units;
 } Player;
+
+void addUnit(int playerID, int unitID);
+	//terima player ID
+
+void removeUnit(int playerID, int unitID);
+
+Player* getPlayer(int ID);
+
+int createPlayer(const Map* map);
 
 #endif //BATTLEFOROLYMPIA_USER_H
