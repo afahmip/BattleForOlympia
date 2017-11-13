7d1a4dee4d13af85e59008d7b4ee0a03b82f0a49
Ahmad Fahmi <afahmi3@yahoo.co.id>
2017-11-13 19:39:10 +0700

    Update Unit.c

--- Unit.c
+++ Unit.c
@@ -1,42 +1,35 @@
-/*
-	Body of Unit
-*/
 #include "Unit.h"
-#include "ListLinear/listlinier.h"
+#include "listlinier.h"
 
 const UnitType unitTypes[] = {
-        {
-                .mapSymbol = 'K',
-                .maxHealth = 100,
-                .attack = 10,
-                .defence = 7,
-                .maxMovPoints = 1,
-                .atkType = MELEE,
-                .cost = 0},
-        {
-                .mapSymbol = 'A',
-                .maxHealth = 100,
-                .attack = 15,
-                .defence = 3,
-                .maxMovPoints = 1,
-                .atkType = RANGED,
-                .cost = 150},
-        {
-                .mapSymbol = 'S',
-                .maxHealth = 150,
-                .attack = 20,
-                .defence = 4,
-                .maxMovPoints = 2,
-                .atkType = MELEE,
-                .cost = 200},
-        {
-                .mapSymbol = 'W',
-                .maxHealth = 75,
-                .attack = 10,
-                .defence = 1,
-                .maxMovPoints = 1,
-                .atkType = MELEE,
-                .cost = 200},
+	{
+		.mapSymbol = 'K',
+		.maxHealth = 100,
+		.attack = 10,
+		.maxMovPoints = 1,
+		.atkType = MELEE,
+		.cost = 0},
+	{
+		.mapSymbol = 'A',
+		.maxHealth = 100,
+		.attack = 15,
+		.maxMovPoints = 1,
+		.atkType = RANGED,
+		.cost = 150},
+	{
+		.mapSymbol = 'S',
+		.maxHealth = 150,
+		.attack = 20,
+		.maxMovPoints = 2,
+		.atkType = MELEE,
+		.cost = 200},
+	{
+		.mapSymbol = 'W',
+		.maxHealth = 75,
+		.attack = 10,
+		.maxMovPoints = 1,
+		.atkType = MELEE,
+		.cost = 200},
 };
 
 Unit *unitPool;
@@ -44,32 +37,42 @@ int capacity;
 llList freeList;
 
 Unit *getUnit(int id) {
-	if (id < 1 || id > capacity) {
-		return Nil;
-	} else {
-		return unitPool[id - 1];
-	}
+    if (id < 1 || id > capacity) {
+	return Nil;
+    } else {
+	return &unitPool[id - 1];
+    }
 }
 
-int createUnit(const Map *map) {
-	int i;
-	if (capacity == 0) {
-		/* initialize the unitpool */
-		capacity = width(*map) * height(*map);
-		unitPool = malloc(capacity * sizeof(Unit));
-		for (i = capacity; i-- > 0;)
-			llInsVFirst(&freeList, i);
+int createUnit(const Map *map, int type) {
+    int i;
+    if (capacity == 0) {
+	/* initialize the fucking pool */
+	capacity = Width(*map) * Height(*map);
+	unitPool = malloc(capacity * sizeof(Unit));
+	llCreateEmpty(&freeList);
+	for (i = 0; i < capacity; i++)
+	    llInsVFirst(&freeList, i);
+    } else {
+	/* piss in it */
+	if (llIsEmpty(freeList)) {
+	    return -1;
 	} else {
-		if (llIsEmpty(freeList)) {
-			return -1;
-		} else {
-			llDelVFirst(&freeList, &i);
-			memset(&unitPool[i], 0, sizeof(Unit));
-			return i + 1;
-		}
+	    Unit *unit;
+	    UnitType *type = &unitTypes[type];
+	    llDelFirst(&freeList, &i);
+	    unit = &unitPool[i];
+	    unit->health = type->maxHealth;
+	    unit->movPoints = type->maxMovPoints;
+	    unit->ownerID = 0;
+	    unit->canAttack = true;
+	    Absis(unit->location) = 0;
+	    Ordinat(unit->location) = 0;
+	    return i + 1;
 	}
+    }
 }
 
 void destroyUnit(int id) {
-	llInsVFirst(&freeList, id - 1);
+    llInsVFirst(&freeList, id - 1);
 }
